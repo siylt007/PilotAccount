854a3edb5c2ddcdd2eed634dfc3a4847353ffc41
shunbaojiang <shunbaojiang@pateo.com.cn>
2018-11-09 17:47:53 +0800

    arm架构脚本引用编译通过
    Change-Id: Id621ca62e7a44b12cfc4c716bc3b1b556dd01c64

--- app/src/main/java/cn/openpilot/account/MainActivity.java
+++ app/src/main/java/cn/openpilot/account/MainActivity.java
@@ -1,13 +1,18 @@
 package cn.openpilot.account;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import butterknife.BindView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    @BindView(R.id.test)
+    RecyclerView mRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
