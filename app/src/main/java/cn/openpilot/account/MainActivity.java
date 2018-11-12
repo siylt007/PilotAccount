c3eb1d8cd283765681c6641fcbdab399eff0c701
shunbaojiang <shunbaojiang@pateo.com.cn>
2018-11-12 20:18:58 +0800

    引入通用的一些处理逻辑准备修改。
    Change-Id: Ib246ac38bd15cdb18d50f2a424d278d299a7b8b2

--- app/src/main/java/cn/openpilot/account/MainActivity.java
+++ app/src/main/java/cn/openpilot/account/MainActivity.java
@@ -1,18 +1,104 @@
 package cn.openpilot.account;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import butterknife.BindView;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.jess.arms.base.BaseActivity;
+import com.jess.arms.di.component.AppComponent;
+import com.tbruyelle.rxpermissions2.RxPermissions;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import cn.openpilot.account.di.component.DaggerUserComponent;
+import cn.openpilot.account.di.module.UserModule;
+import cn.openpilot.account.mvp.contract.UserContract;
+import cn.openpilot.account.mvp.presenter.UserPresenter;
+
+public class MainActivity extends BaseActivity<UserPresenter> implements UserContract.View {
 
     @BindView(R.id.test)
-    RecyclerView mRecyclerView;
+    public TextView mText;
+
+
+
+    @OnClick(R.id.test)
+    public void showToast(){
+        Toast.makeText(this, "is a click", Toast.LENGTH_SHORT).show();
+        mPresenter.requestUsers(true);
+
+    }
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void setupActivityComponent(@NonNull AppComponent appComponent) {
+
+       // DaggerUserComponent.builder().appComponent(appComponent).userModule(new UserModule(this)).build().inject(this);
+        DaggerUserComponent
+                .builder()
+                .appComponent(appComponent)
+                .userModule(new UserModule(this))
+                .build()
+                .inject(this);
+
+    }
+
+    @Override
+    public int initView(@Nullable Bundle savedInstanceState) {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initData(@Nullable Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public void startLoadMore() {
+
+    }
+
+    @Override
+    public void endLoadMore() {
+
+    }
+
+    @Override
+    public Activity getActivity() {
+        return null;
+    }
+
+    @Override
+    public RxPermissions getRxPermissions() {
+        return null;
+    }
+
+    @Override
+    public void showLoading() {
+
+    }
+
+    @Override
+    public void hideLoading() {
+
+    }
+
+    @Override
+    public void showMessage(@NonNull String message) {
+
+    }
+
+    @Override
+    public void launchActivity(@NonNull Intent intent) {
+
+    }
+
+    @Override
+    public void killMyself() {
 
     }
 }
